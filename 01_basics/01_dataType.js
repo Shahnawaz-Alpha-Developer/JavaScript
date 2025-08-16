5510701dc6f9aeee8ec2efe3600c0da2cc627b3f
Shahnawaz-Alpha-Developer <shahnawazkhan12.2007@gmail.com>
2025-08-16 16:01:10 +0000

    data type

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+//alert('Shahnawaz KHan') // we are using node js not browser
+
+console.log(5 
+    * 5) // code readiability should be high
+console.log('shahnawaz Khan');
+
+
+let name = "Shahnawaz" //string
+let age  = 18 //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+// number => 2 to power 53
+// bigInt =>
+// string => ""
+// boolean => true/false
+// null => standalone value (khali)
+// undefined => value is not asigned yet
+// symbol => unique
+
+// object
+
+//finding the data type of variable
+console.log(typeof null) // object
+console.log(undefined); // undefined
+
+
+
+
+
+

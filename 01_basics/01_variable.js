6e490028b0eac9204504c8cfcad32886e4bd6b9d
Shahnawaz-Alpha-Developer <shahnawazkhan12.2007@gmail.com>
2025-08-16 15:32:41 +0000

    basics of JavaScript

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -22,6 +22,7 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 
+//console log is not good practise for multiple printing best we can use this.
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 
